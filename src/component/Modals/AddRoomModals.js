4007c90845001ca11521870bdd7a47a501eab197
phuquoc <quocduongphu@gmail.com>
2021-07-23 15:45:32 +0700

    update

--- src/component/Modals/AddRoomModals.js
+++ src/component/Modals/AddRoomModals.js
@@ -8,12 +8,12 @@ import { addDocument } from '../../firebase/services';
 export default function AddRoomModals() {
     const {isAddRoomVisible} = useContext(AppContext);
     const {setIsAddRoomVisible} = useContext(AppContext);
-    const  {user}  = useContext(AuthContext);
+    const  {user : {uid}}  = useContext(AuthContext);
     const [form] = useForm();
 
     const handleOk = () => {
        
-        addDocument('rooms', {...form.getFieldsValue(), members: [user.uid] })
+        addDocument('rooms', {...form.getFieldsValue(), members: [uid] })
         
         form.resetFields();
 
